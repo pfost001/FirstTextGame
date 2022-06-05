491bb00d80826c7cd724dff1b7d4557f1a49a2f6
Paul <Paul@DESKTOP-JESKJOS>
2022-06-04 21:59:48 -0700

    Add project files.

---
+++ IndividualProject/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualProject
+{
+    [Serializable] //this means the data in here can be serialized and saved. it comes from the system.serialization call in the program. 
+    public class Player
+    {
+        Random rand = new Random();
+        public string Name;
+        public int SaveCounter = 0;
+        public int coins = 50000;
+        public int health = 10;
+        public int damage = 1;
+        public int armorValue = 0;
+        public int potion = 5;
+        public int weaponValue = 5;
+        public int mods = 1;
+
+        public int GetStatPower()
+        {
+            //this is to buy mods to make the game harder
+            int upper = (2 * mods + 2);
+            int lower = (mods + mods/3);
+            return rand.Next(lower, upper);
+        }
+        public int GetStatHealth()
+        {
+            //this is to buy mods to give monsters more health
+            int upper = (3 * mods + 5);
+            int lower = (mods);
+            return rand.Next(lower, upper);
+        }
+        public int GetCoin()
+        {
+            int upper = (5 * mods + 9);
+            int lower = (mods + 3);
+            return rand.Next(lower, upper);
+        }
+            
+
+    }
+}
---
+++ IndividualProject/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace IndividualProject
+{
+    public class Program
+    {
+        public static Player currentPlayer = new Player();
+        public static bool mainLoop = true;
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists("saves")) //this will check the directory for a folder named saves. it is where the saved files from player.cs will go. Notice the !
+            {
+                Directory.CreateDirectory("saves"); //this will make the directory if "saves" does not exist.
+            }
+            Start();
+            Encouter.FirstEncounter();
+            while (mainLoop)
+            {
+                Encouter.RandomEncounter();
+            }
+            
+        }
+        static void Start()
+        {
+            Console.WriteLine("Foto Farmer");
+            Console.WriteLine("What is your name?");
+            currentPlayer.Name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("This is where you put the intro to the story.");
+            Console.WriteLine("You can write more intro story here.");
+            if (currentPlayer.Name == "")
+            {
+                Console.WriteLine("It is too bad you don't have a name for your farm!");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to " + currentPlayer.Name + "'s Farm!");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("This is more story if you need it.");
+        }
+        public static void Save()
+        {
+            Console.Clear();
+            Console.WriteLine("Would you like to (o)verwrite or (s)ave a new file?");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "o")
+            {
+                BinaryFormatter binForm = new BinaryFormatter();
+                string path = "saves/" + currentPlayer.Name.ToLower() + "_" + Program.currentPlayer.SaveCounter; //sets the filename for saving to. Using current player name. need to format checks on the enter name box.
+                FileStream file = File.Open(path, FileMode.OpenOrCreate);
+                binForm.Serialize(file, currentPlayer);
+                file.Close();                
+            }
+            else
+            {
+                Program.currentPlayer.SaveCounter++;
+                BinaryFormatter binForm = new BinaryFormatter();
+                string path = "saves/" + currentPlayer.Name.ToLower(); //sets the filename for saving to. Using current player name. need to format checks on the enter name box.
+                FileStream file = File.Open(path, FileMode.OpenOrCreate);
+                binForm.Serialize(file, currentPlayer);
+                file.Close();
+                
+            }
+        }
+
+        public static Player Load()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose your saved game");
+            string[] paths = Directory.GetDirectories("saves"); //this will build an array from all the file names in the "saves" folder
+            List<Player> players = new List<Player>(); //creates a new list of the players
+            
+            BinaryFormatter binForm = new BinaryFormatter();
+            foreach (string p in paths)
+            {
+                FileStream file = File.Open(p, FileMode.Open); //do not use opne or create or it will pull the file as writable
+                Player pfile = (Player)binForm.Deserialize(file); //use cast to be sure you are opening only a player file. deserialize means load.
+                file.Close();
+                players.Add(pfile); //this will add the pfile to the players list
+            }
+            while (true) 
+            {
+                Console.Clear();
+                Console.WriteLine("Current Save Files:");
+                foreach (Player p in players)
+                {
+                    Console.WriteLine("Name: "p.Name + ", Save Number:" + p.SaveCounter);
+                }
+                Console.WriteLine("Please input player name.");
+                string inputfilename = Console.ReadLine();
+                Console.WriteLine("Please input save number.");
+                string inputsavenum = Console.ReadLine();
+                try
+                {
+                    if (inputfilename == )
+                }
+                catch
+                {
+                    Console.WriteLine("FILE NOT FOUND! Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+    }
+}
